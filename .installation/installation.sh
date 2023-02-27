7afa6c301e4da052542bd6e786f57700ee54b1ac
Uriel <83469583+JustAsteri@users.noreply.github.com>
2023-02-27 10:40:57 -0600

    Update installation.sh

--- .installation/installation.sh
+++ .installation/installation.sh
@@ -13,7 +13,7 @@ sudo pacman-mirrors -g &&                         # Update mirrors
 sudo pacman -Syyu &&                              # Update system
 xargs -a packages.txt sudo pacman -S &&           # Install pacman packages
 xargs -a yay_packages.txt yay -S &&               # Install yay packages
-# sh ~/dotfiles/.installation/make-symlinks.sh && # Make symlinks for dotfiles
+sh ~/dotfiles/.installation/make-symlinks.sh && # Make symlinks for dotfiles
 sudo ln -s /var/lib/snapd/snap /snap &&           # Snap
 sudo pacman -Rsn $(pacman -Qdtq) &&               # Remove unused packages
 # sh manual_installation.sh &&                      # Install packages manually
