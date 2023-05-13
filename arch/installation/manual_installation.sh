941eb6e4c757e078c2cf01d8c136a52d0e55cfda
Uriel <83469583+JustAsteri@users.noreply.github.com>
2023-05-12 18:25:11 -0600

    cambios

--- arch/installation/manual_installation.sh
+++ arch/installation/manual_installation.sh
@@ -28,25 +28,6 @@ rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
-# <Espanso>
-
-cd
-# Create the $HOME/opt destination folder
-mkdir -p ~/opt
-# Download the AppImage inside it
-wget -O ~/opt/Espanso.AppImage 'https://github.com/federico-terzi/espanso/releases/download/v2.1.8/Espanso-X11.AppImage'
-# Make it executable
-chmod u+x ~/opt/Espanso.AppImage
-# Create the "espanso" command alias
-sudo ~/opt/Espanso.AppImage env-path register
-
-# Register espanso as a systemd service (required only once)
-espanso service register
-
-# Start espanso
-espanso start
-# <Espanso/>
-
 # rofi themes
 cd
 git clone --depth=1 https://github.com/JustAsteri/rofi.git
@@ -57,11 +38,6 @@ chmod +x setup.sh
 # TMUX plugins
 cd
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Neofetch themes
-cd ~/.config/neofetch/ && rename config.conf configbackup.conf config.conf
-git clone https://github.com/Chick2D/neofetch-themes/
-cat neofetch-themes/small/blockfetch.conf > ~/.config/neofetch/config.conf
 
 # P10K
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
